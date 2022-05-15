1ea864eedcd1bf923061f85c4907694a3ece9320
dangelomedinag <dangelomedinag@gmail,com>
2022-05-15 10:28:42 -0400

    update adapter

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,7 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		trailingSlash: 'never'
+		adapter: adapter()
 	}
 };
 
