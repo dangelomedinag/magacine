a57cc4a665007c101cd35cb55651899056fd7dd8
dangelomedinag <dangelomedinag@gmail,com>
2022-05-15 17:49:49 -0400

    revert config for edge function

--- src/hooks.js
+++ src/hooks.js
@@ -1,12 +1,12 @@
-/** @type {import('@sveltejs/kit').Handle} */
-export function handle({ event, resolve }) {
-	for (const param in event.params) {
-		const value = event.params[param];
-		if (value === 'favicon.png') {
-			console.log('src/hooks.js', value);
-			new Response('Ugg!  One of those extra requests again!', { status: 400 });
-		}
-	}
+// /** @type {import('@sveltejs/kit').Handle} */
+// export function handle({ event, resolve }) {
+// 	for (const param in event.params) {
+// 		const value = event.params[param];
+// 		if (value === 'favicon.png') {
+// 			console.log('src/hooks.js', value);
+// 			new Response('Ugg!  One of those extra requests again!', { status: 400 });
+// 		}
+// 	}
 
-	return resolve(event);
-}
+// 	return resolve(event);
+// }
