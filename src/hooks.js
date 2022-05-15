ffe9486157f9f564f454cd72bbace8595a8d5d02
dangelomedinag <dangelomedinag@gmail,com>
2022-05-15 17:22:03 -0400

    new

--- src/hooks.js
+++ src/hooks.js
@@ -3,7 +3,7 @@ export function handle({ event, resolve }) {
 	for (const param in event.params) {
 		const value = event.params[param];
 		if (value === 'favicon.png') {
-			console.log(value);
+			console.log('src/hooks.js', value);
 			new Response('Ugg!  One of those extra requests again!', { status: 400 });
 		}
 	}
