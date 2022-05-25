e794f2714072766bf9568d4b32fe089763f42b10
dangelomedinag <dangelomedinag@gmail,com>
2022-05-25 01:34:28 -0400

    implemented new '/soon' route for next releases

---
+++ src/routes/soon.svelte
@@ -0,0 +1,38 @@
+<script context="module">
+	export async function load({ fetch }) {
+		const req = await fetch('/api?s=america&year=2022');
+		if (!req.ok) {
+			const res = await req.json();
+			return { status: req.status, error: new Error(res.message) };
+		}
+
+		const res = await req.json();
+
+		return {
+			props: {
+				movies: res
+			}
+		};
+	}
+</script>
+
+<script>
+	import CarouselMovies from '$components/ui/CarouselMovies.svelte';
+	import NavbarTop from '$components/ui/NavbarTop.svelte';
+	export let movies;
+</script>
+
+<NavbarTop />
+
+<div class="content">
+	<h1>Next Releases</h1>
+
+	<CarouselMovies {movies} title="comming soom" />
+</div>
+
+<style>
+	h1 {
+		font-size: 2rem;
+		/* margin-bottom: 0.5em; */
+	}
+</style>
