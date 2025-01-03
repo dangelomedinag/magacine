bce592273e05f48ecc11bb6e46798a7f221a7721
dangelomedinag <dangelomedinag@gmail.com>
2025-01-03 14:40:11 -0300

    chore update deps

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,8 @@
 <script>
-	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import '../app.css';
-	// import 'normalize.css';
 	import '../styles/_global.css';
 	import { dev } from '$app/environment';
 	import MediaQueries from '$components/dev/mediaQueries.svelte';
-	injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 	/** @type {{children?: import('svelte').Snippet}} */
 	let { children } = $props();
