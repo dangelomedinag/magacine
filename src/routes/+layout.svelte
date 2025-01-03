c209ec90f9cd490e69d575875e7b05c6a5136e6c
dangelomedinag <dangelomedinag@gmail.com>
2025-01-03 13:37:24 -0300

    feat: vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import '../app.css';
 	// import 'normalize.css';
 	import '../styles/_global.css';
 	import { dev } from '$app/environment';
 	import MediaQueries from '$components/dev/mediaQueries.svelte';
+	injectAnalytics();
 
 	/** @type {{children?: import('svelte').Snippet}} */
 	let { children } = $props();
