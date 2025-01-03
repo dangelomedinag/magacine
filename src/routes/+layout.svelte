03305c573077cf6f872a01c789a48335d8e74d43
dangelomedinag <dangelomedinag@gmail.com>
2025-01-03 13:15:20 -0300

    update deps

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,8 @@
 <script>
 	import 'normalize.css';
-	import { dev } from '$app/environment';
-	import NProgress from 'nprogress';
-	import MediaQueries from '$components/dev/mediaQueries.svelte';
-	import 'nprogress/nprogress.css';
-	import { onMount } from 'svelte';
-	import { themeStore } from '$lib/stores/theme-store';
-	import { afterNavigate, beforeNavigate } from '$app/navigation';
 	import '../styles/_global.css';
+	import { dev } from '$app/environment';
+	import MediaQueries from '$components/dev/mediaQueries.svelte';
 
 	/** @type {{children?: import('svelte').Snippet}} */
 	let { children } = $props();
